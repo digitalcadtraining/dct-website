1cb64ab650d500b6aa6e4dbc3c757f7dcf4bbd07
digitalcadtraining <digitalcadtraining@gmail.com>
2026-06-19 19:11:11 +0530

    Mount homepage runtime scroll fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import AppRoutes from "./routes/AppRoutes.jsx";
+import { HomePageRuntimeFixes } from "./pages/HomePageRuntimeFixes.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
+        <HomePageRuntimeFixes />
         <AppRoutes />
       </AuthProvider>
     </BrowserRouter>
