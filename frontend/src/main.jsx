1b04e6b3ec7df32ed02cee52a366d69482cac6ee
digitalcadtraining <digitalcadtraining@gmail.com>
2026-06-20 20:03:13 +0530

    Load mobile-only course page polish CSS

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/index.css";
 import "./styles/home-mobile-polish.css";
+import "./styles/course-mobile-polish.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
